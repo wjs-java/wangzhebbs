d876d51243658bb2e4d1e413bf8cce31681a5d0d
lanjainhua <1135790798@qq.com>
2019-06-24 18:31:42 +0800

    蓝建华提交测试

--- royal_bbs/royal_bbs_web/src/main/java/com/bbs/controller/UserController.java
+++ royal_bbs/royal_bbs_web/src/main/java/com/bbs/controller/UserController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-/*测试*/
+
 @Controller
 @RequestMapping("/user")
-/*112233*/
+/*11223333*/
 public class UserController {
     @Autowired
     private UserService userService;
